e6c6608afe0c0ca768a3bdba74346f43b999f4b0
pom7 <jijunkang666@icloud.com>
2020-01-28 11:23:11 +0800

    index

--- src/main/java/life/jjk/community/controller/HelloController.java
+++ src/main/java/life/jjk/community/controller/HelloController.java
@@ -1,4 +1,4 @@
-package life.jjk.community;
+package life.jjk.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/life/jjk/community/controller/IndexController.java
@@ -0,0 +1,10 @@
+package life.jjk.community.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
